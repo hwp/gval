357e8c7971f720212b972286cffd5bc7c5f40187
Weipeng He <heweipeng@gmail.com>
2014-11-24 16:01:28 +0100

    MFCC function

--- src/gval_utils.c
+++ src/gval_utils.c
@@ -4,20 +4,20 @@
  * gval_utils.c : Utility Functions
  *
  * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General 
- * Public License as published by the Free Software 
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
- * This program is distributed in the hope that it will 
+ * This program is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  *
- * You should have received a copy of the GNU General 
+ * You should have received a copy of the GNU General
  * Public License along with this program; if not, write
- * to the Free Software Foundation, Inc., 51 Franklin 
+ * to the Free Software Foundation, Inc., 51 Franklin
  * Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
@@ -54,7 +54,7 @@ void gval_spectrum(gdouble* result, const gdouble* signal,
 
   // Calculate spectrum
   for (i = 0; i < size / 2 + 1; i++) {
-    result[i] = sqrt(dft[i][0] * dft[i][0] 
+    result[i] = sqrt(dft[i][0] * dft[i][0]
         + dft[i][1] * dft[i][1]) / (double) size;
   }
 }
@@ -62,6 +62,55 @@ void gval_spectrum(gdouble* result, const gdouble* signal,
 void gval_mfcc(gdouble* result, const gdouble* signal,
     guint size, guint n_channels, guint spl_rate,
     window_func_t window) {
-  
+  guint i, j;
+
+  // Power spectrum
+  gdouble* spec = g_malloc_n(size / 2 + 1,
+      sizeof(gdouble));
+  gval_spectrum(spec, signal, size, window);
+
+  // Plan for dct
+  double* buf = fftw_alloc_real(n_channels);
+  fftw_plan plan = fftw_plan_r2r_1d(n_channels, buf, buf, FFTW_REDFT10, FFTW_ESTIMATE);
+
+  // Filter bank with mel scale
+  double f_max = spl_rate / 2.0;
+  double f_unit = (double) spl_rate / size;
+  double f_begin = 0.0;
+  double f_mid = 700.0 * (pow(1.0 + f_max / 700.0,
+        1.0 / (n_channels + 1.0)) - 1.0);
+  double f_end = 700.0 * (pow(1.0 + f_max / 700.0,
+        2.0 / (n_channels + 1.0)) - 1.0);
+  for (i = 0; i < n_channels; i++) {
+    double sum = 0.0;
+    for (j = (guint) ceil(f_begin / f_unit);
+        j< (guint) floor(f_mid / f_unit); j++) {
+      assert(j >= 0 && j < size / 2 + 1);
+      sum += spec[j] * (j * f_unit - f_begin)
+        / (f_mid - f_begin);
+    }
+    for ( ; j< (guint) floor(f_end / f_unit); j++) {
+      assert(j >= 0 && j < size / 2 + 1);
+      sum += spec[j] * (f_end - j * f_unit)
+        / (f_end - f_mid);
+    }
+
+    // log of filter bank
+    buf[i] = log(sum);
+  }
+
+  g_free(spec);
+
+  // calculate dct
+  fftw_execute(plan);
+
+  fftw_destroy_plan(plan);
+
+  // copy result
+  for (i = 0; i < n_channels; i++) {
+    result[i] = buf[i];
+  }
+
+  fftw_free(buf);
 }
 
