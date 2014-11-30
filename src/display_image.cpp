b910e020cd5cd53d89d7dae44b97a5754e6ce1dd
Weipeng He <heweipeng@gmail.com>
2014-11-30 12:58:59 +0100

    minor

--- src/display_image.cpp
+++ src/display_image.cpp
@@ -4,11 +4,12 @@
 // Author : Weipeng He <heweipeng@gmail.com>
 // Copyright (c) 2014, All rights reserved.
 
-#include <opencv2/core/core.hpp>
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/nonfree/features2d.hpp"
-#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 
